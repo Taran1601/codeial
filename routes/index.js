e54acafe6d0c76cc17a49a6ba47b857e8928be6a
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-03 02:30:25 +0530

    added users/profile controller

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express= require('express');
 const router= express.Router();
 const homeController= require('../controllers/home_controller');
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
-
-
+//for any other routes, access from here
+//router.use('/routerName',require('./routerFile'));
 
 module.exports = router;
