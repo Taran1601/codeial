50c88481834ed79c8009e29ad5ba0ee94ca38d79
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-10 03:01:57 +0530

    added css to layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 
 app.use(expressLayouts);
-
+app.use(express.static('./assets'));
 //use express router
 app.use('/',require('./routes'));
 //set up the view engine
