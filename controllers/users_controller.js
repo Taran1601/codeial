ecf085c07f98bc97fa154ee0a7ef75fd28bb7f5a
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-12 23:36:16 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,20 @@
 const User=require('../models/user');
 module.exports.profile=function(req,res){
-    return res.render('user_profile',{
-title:"user profile"
+//     return res.render('user_profile',{
+// title:"user profile"
+if(req.cookies.user_id){
+    User.findById(req.cookies.user_id,function(err,user){
+        if(user){
+return res.render('user_profile',{
+   title:"Usres Profile",
+   user:user
+});
+        }
+        return res.redirect('/users/sign-in');
     });
+}else{
+return res.redirect('users/sign-in');
+}
 }
 
 //render the sign up page
@@ -26,7 +38,7 @@ module.exports.create=function(req,res){
         email:req.body.email
     },function(err,user){
 if(err){
-    console.log('error in finding a user while signing in');
+    console.log('error in finding a user while signing up');
 }
 if(!user){
     User.create(req.body,function(err,user){
@@ -43,5 +55,31 @@ else{
 
 }
 module.exports.createSession=function(req,res){
-    //TODO later
+    //steps to authenticate
+    //find the user
+User.findOne({
+    email:req.body.email
+},function(err,user){
+    if(err){
+        console.log('error in finding a user while signing in');
+        return;
+    }
+     //handle user found
+    if(user){
+//handle password which don't match
+if(user.password!=req.body.password){
+    return res.redirect('back');
+}
+//handle session creation
+res.cookie('user_id',user.id);
+return res.redirect('/users/profile');
+    }else{
+ //handle user not hound
+ return res.redirect('back');
+    }
+});  
+}
+module.exports.signOut=function(req,res){
+    res.clearCookie('user_id');
+    return res.redirect('/users/sign-in');
 }
