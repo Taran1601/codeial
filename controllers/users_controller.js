900b5e72bb45830d23d040106fc2b9b1bed70a2a
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-12 15:09:23 +0530

    Y

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,16 @@ module.exports.profile=function(req,res){
 title:"user profile"
     });
 }
+
+//render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    });
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    });
+}
