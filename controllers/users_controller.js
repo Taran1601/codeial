2f3061ec661c73cf4e85b4c9b8a67a7aa31367ce
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-12 21:42:34 +0530

    created a user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.profile=function(req,res){
     return res.render('user_profile',{
 title:"user profile"
@@ -18,7 +19,28 @@ module.exports.signIn=function(req,res){
 }
 //get the sign up data
 module.exports.create=function(req,res){
-    //TODO later
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({
+        email:req.body.email
+    },function(err,user){
+if(err){
+    console.log('error in finding a user while signing in');
+}
+if(!user){
+    User.create(req.body,function(err,user){
+      if(err){
+        console.log('error in creating a user while signing up');
+      }
+      return res.redirect('/users/sign-in');
+    });
+}
+else{
+    return res.redirect('back');
+}
+    });
+
 }
 module.exports.createSession=function(req,res){
     //TODO later
