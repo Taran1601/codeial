107e2227dea06b08f72dc1f0e4ddfeae66cdfd83
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-15 03:01:44 +0530

    passport

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,46 @@
+const passport=require('passport');
+
+const LocalStrategy=require('passport-local').Strategy;
+
+const User=require('../models/user');
+
+//authentication using passport
+passport.use(new LocalStrategy({
+    //how to detect a user(unique)
+usernameField:'email'
+},
+function(email,password,done){
+//find a user and establish the identity
+User.findOne({email:email},function(err,user){
+if(err){
+    console.log('error in finding user--->Passport');
+    return done(err);
+}
+if(!user || user.password!=password){
+    console.log('Invalid username/Password');
+    return done(null,false);
+}
+return done(null,user);
+});
+
+}
+));
+
+
+////serailizing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user,done){
+done(null,user.id);
+});
+
+
+//deserializing the user from the key in the cookie
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if(err){
+            console.log('error in finding user from passport');
+        }
+        return done(null);
+    });
+});
+module.exports=passport;
+                 
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
+//used for session cookie
+const session=require('express-session');
+const passport=require('passport');
+const LocalStrategy=require('passport-local');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
