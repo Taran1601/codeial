5c495bda9249ccfcb93b708082010a53a3d4355c
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-23 02:15:04 +0530

    author of post added

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,19 @@
+const Post=require('../models/post');
 module.exports.home=function(req,res){
     console.log(req.cookies);
     // res.cookies('user_id',25);
-    // return res.end('<h1>Express is up for Codeial</h1>');
-    return res.render('home',{
-        title: "Home"
+//   Post.find({},function(err,posts){
+//     return res.render('home',{
+//         title: " Codeial | Home",
+//         posts:posts
+//   });
+//     });
+    //populate the user of each post
+    Post.find({}).populate('user').exec(function(err,posts){
+       return res.render('home',{
+            title: " Codeial | Home",
+            posts:posts
+      });
     });
 }
 
