e56a91f65895b90721ad0c85c9bbb37c43ce9d85
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-22 22:10:32 +0530

    create a post

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router= express.Router();
 const homeController= require('../controllers/home_controller');
 router.get('/',homeController.home);
 router.use('/users',require('./users'));
-
+router.use('/posts',require('./posts'));
 //for any other routes, access from here
 //router.use('/routerName',require('./routerFile'));
 
