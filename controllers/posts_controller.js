d5863e7a21e4d4552b34aa77a5800e60c893d5c8
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-02-26 14:50:17 +0530

    deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment');
 module.exports.create=function(req,res){
 Post.create({
     content:req.body.content,
@@ -9,4 +10,20 @@ Post.create({
     }
     return res.redirect('back');
 });
+}
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+        //.id means automatically converting the id of the user into string
+        if(post.user==req.user.id){
+            post.remove();
+            Comment.deleteMany({
+                post:req.params.id
+            },function(err){
+                return res.redirect('back');
+            });
+        }else{
+            res.redirect('back');
+        }
+      
+    });
 }
