c2a5566403031dbadc87ef347aa49d7e41990611
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-03-03 21:45:02 +0530

    made async functions

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,29 +1,55 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
-module.exports.create=function(req,res){
-Post.create({
+module.exports.create= async function(req,res){
+// Post.create({
+//     content:req.body.content,
+//     user:req.user._id
+// },function(err,post){
+//     if(err){
+//         console.log('error in creating a post');
+//     }
+//     return res.redirect('back');
+// });
+try{
+  await Post.create({
     content:req.body.content,
     user:req.user._id
-},function(err,post){
-    if(err){
-        console.log('error in creating a post');
-    }
-    return res.redirect('back');
-});
+}); 
+return res.redirect('back');
+}catch(err){
+    console.log('Error',err);
+    return;
 }
-module.exports.destroy=function(req,res){
-    Post.findById(req.params.id,function(err,post){
-        //.id means automatically converting the id of the user into string
+}
+module.exports.destroy= async function(req,res){
+    // Post.findById(req.params.id,function(err,post){
+    //     //.id means automatically converting the id of the user into string
+    //     if(post.user==req.user.id){
+    //         post.remove();
+    //         Comment.deleteMany({
+    //             post:req.params.id
+    //         },function(err){
+    //             return res.redirect('back');
+    //         });
+    //     }else{
+    //         res.redirect('back');
+    //     }
+      
+    // });
+    try{
+ let post=await Post.findById(req.params.id);
         if(post.user==req.user.id){
             post.remove();
-            Comment.deleteMany({
+             await Comment.deleteMany({
                 post:req.params.id
-            },function(err){
-                return res.redirect('back');
             });
+            return res.redirect('back');
         }else{
             res.redirect('back');
-        }
-      
-    });
+        }   
+    }
+    catch(err){
+console.log('Error',err);
+return;
+    }
 }
