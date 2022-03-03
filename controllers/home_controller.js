5e6aa87ea913b80a88889f5762fa977ef9a68811
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-03-03 20:51:23 +0530

    made home controller function as async

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post=require('../models/post');
 const User=require('../models/user');
-module.exports.home=function(req,res){
-    console.log(req.cookies);
+module.exports.home= async function(req,res){
+    // console.log(req.cookies);
     // res.cookies('user_id',25);
 //   Post.find({},function(err,posts){
 //     return res.render('home',{
@@ -9,25 +9,40 @@ module.exports.home=function(req,res){
 //         posts:posts
 //   });
 //     });
-    //populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate:{
-            path:'user'
-        }
-    })
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title: " Codeial | Home",
-                posts:posts,
-             all_users:users
-          });
-        });
-      
-    });
+try{
+ //populate the user of each post
+ let posts= await Post.find({})
+ .populate('user')
+ .populate({
+     path:'comments',
+     populate:{
+         path:'user'
+     }
+ });
+ // // .exec(function(err,posts){
+ // //     User.find({},function(err,users){
+ // //         return res.render('home',{
+ // //             title: " Codeial | Home",
+ // //             posts:posts,
+ // //          all_users:users
+ // //       });
+ // //     });
+   
+ // });
+ let users=await User.find({});
+  return res.render('home',{
+title: " Codeial | Home",
+   posts:posts,
+    all_users:users
+           });
+         
 }
-
+catch(err){
+console.log('Error',err);
+}
+} 
 //module.exports.actionName=function(req,res){}
+//using then
+//  let posts=Post.find({}).populate('comments').then(function());
+// let posts=Post.find({}).populate('comments').exec();
+// posts.then()
