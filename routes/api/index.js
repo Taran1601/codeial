bb04d60a8ee523e533a2ceaa2b97abbb8bda820b
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-03-08 16:15:24 +0530

    created routes and controller for api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 //this file is for handling all api routes like api/v1
 const express= require('express');
 const router= express.Router();
-
+router.use('/v1',require('./v1'));
 module.exports=router;
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,1 +1,5 @@
 //this file is for handling all the routes for a particular version like api/v1/posts
+const express= require('express');
+const router= express.Router();
+router.use('/posts',require('./posts'));
+module.exports=router;
