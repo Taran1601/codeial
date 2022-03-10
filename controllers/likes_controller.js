cbc7aac2659b58be1583ea46d1ba193c8afd82e0
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-03-11 02:32:45 +0530

    likes controller

---
+++ controllers/likes_controller.js
@@ -0,0 +1,53 @@
+const Like=require('../models/likes');
+const Comment=require('../models/comment');
+const Post=require('../models/post');
+
+
+module.exports.toggleLike=async function(req,res){
+    try{
+//likes/toggle/?id=abcdef&type=Post
+let likeable;
+let deleted=false;
+
+if(req.query.type=='Post'){
+    likeable=await Post.findById(req.query.id).populate('likes');
+}else{
+    likeable=await Comment.findById(req.query.id).populate('likes');
+}
+//check whether a post/comment is liked or not
+let existingLike=await Like.findOne({
+    likeable:req.query.id,
+    onModel:req.query.type,
+    user:req.user._id
+});
+//if a like already exist then delete it else add it
+if(existingLike)
+{
+    likeable.likes.pull(existingLike._id);
+    likeable.save();
+    existingLike.remove();
+    deleted=true;
+}else{
+    //make a new like
+    let newLike=await Like.create({
+        user:req.user._id,
+        likeable:req.query.id,
+        onModel:req.query.type
+    });
+    likeable.likes.push(newLike._id);
+    likeable.save();
+}
+return res.json(200,{
+    message:"Request successfull",
+    data:{
+deleted:deleted
+    }
+})
+    }
+    catch(err){
+    console.log('error in liking a post or a comment',err);
+    return res.json(500,{
+        message:'Internal Server Error'
+    });
+    }
+}
