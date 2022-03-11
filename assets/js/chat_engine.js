ebdae3491e13e4508edc97a7ba77adc87da0b3d0
Taran1601 <taranpreet1105.cse19@chitkara.edu.in>
2022-03-11 16:20:14 +0530

    sent and received messages

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -30,5 +30,34 @@ class ChatEngine{
                 console.log('user joined',data);
             });
         });
+        //CHANGE:: send a message on clicking the send message button
+        $('#send-message').click(function(){
+            let msg=$('#chat-message-input').val();
+            if(msg!=''){
+self.socket.emit('send_message',{
+    message:msg,
+    user_email:self.userEmail,
+    chatroom:'Codeial'
+});
+            }
+        });
+        //detect if the message is received
+        self.socket.on('receive_message',function(data){
+            console.log('message received',data.message);
+            let newMessage=$('<li>');
+            //let the message be coming from other user
+            let messageType='other-message';
+            if(data.user_email==self.userEmail){
+                messageType='self-message';
+            }
+            newMessage.append($('<span>',{
+                'html':data.message
+            }));
+            newMessage.append($('<sub>',{
+                'html':data.user_email
+            }));
+            newMessage.addClass(messageType);
+            $('#chat-messages-list').append(newMessage);
+                });
     }
 }
